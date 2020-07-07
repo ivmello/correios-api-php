aecb67055cbfb54730ca440cf47a0f1cec42f3fb
Igor Vieira de Mello <igor@shapeweb.com.br>
2020-07-07 15:55:14 -0400

    added routes for CEP and frete

--- app/routes.php
+++ app/routes.php
@@ -17,14 +17,29 @@ return function (App $app) {
     });
 
     $app->get('/', function (Request $request, Response $response) {
-        $response->getBody()->write('Hello world!');
-        return $response;
+        $response->getBody()->write(json_encode(array(
+            "data" => "Bem-vindo a API Correios",
+        )));
+        return $response->withHeader('Content-Type', 'application/json');
     });
 
     $app->get('/cep/{cep}', function (Request $request, Response $response, $args) {
         $cep = $args['cep'];
         $correios = new Client;
         $result = $correios->zipcode()->find($cep);
+        $response->getBody()->write(json_encode($result));
+        return $response->withHeader('Content-Type', 'application/json');
+    });
+
+    $app->get('/frete', function (Request $request, Response $response, $args) {
+        $params = $request->getQueryParams();
+        $correios = new Client;
+        $result = $correios->freight()
+            ->origin($params['cep_origem'])
+            ->destination($params['cep_destino'])
+            ->services(Service::SEDEX, Service::PAC)
+            ->item(16, 16, 16, $params['peso'], 1)
+            ->calculate();
         $response->getBody()->write(json_encode($result));
         return $response->withHeader('Content-Type', 'application/json');
     });
