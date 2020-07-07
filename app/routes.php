c31401daa9f5476ede4acb637bc8658c6fcd15d2
Igor Vieira de Mello <igor@shapeweb.com.br>
2020-07-07 15:47:03 -0400

    added Correios lib

--- app/routes.php
+++ app/routes.php
@@ -7,6 +7,8 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+use FlyingLuscas\Correios\Client;
+use FlyingLuscas\Correios\Service;
 
 return function (App $app) {
     $app->options('/{routes:.*}', function (Request $request, Response $response) {
@@ -17,6 +19,14 @@ return function (App $app) {
     $app->get('/', function (Request $request, Response $response) {
         $response->getBody()->write('Hello world!');
         return $response;
+    });
+
+    $app->get('/cep/{cep}', function (Request $request, Response $response, $args) {
+        $cep = $args['cep'];
+        $correios = new Client;
+        $result = $correios->zipcode()->find($cep);
+        $response->getBody()->write(json_encode($result));
+        return $response->withHeader('Content-Type', 'application/json');
     });
 
     $app->group('/users', function (Group $group) {
